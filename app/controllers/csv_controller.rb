70858004f60c0d72c8ad0c8743daa0405f98b33a
Jason Ronallo <jronallo@gmail.com>
2011-02-12 16:15:45 -0500

    add organization as something that gets reused. simplify error messages by combining them.

--- app/controllers/csv_controller.rb
+++ app/controllers/csv_controller.rb
@@ -9,12 +9,6 @@ class CsvController < ApplicationController
       @upload = Upload.new(params[:upload])
       UseMailer.use_email(@upload).deliver
       
-      if @upload.csv.nil?
-        flash[:notice] = 'You did not upload a CSV file. Please choose a file.'
-        redirect_to csv_import_path
-        return
-      end      
-      
       if @upload.valid?
         @upload.csv.rewind
         ead_generator = Stead::EadGenerator.from_csv(@upload.csv.read)
@@ -27,14 +21,11 @@ class CsvController < ApplicationController
       Rails.logger.error([e.inspect, e.backtrace.join("\n")].join(''))
       flash[:notice] = 'There was an error processing the CSV file. ' +
       'It may be an error in your CSV file or a bug within the program. ' +
-      'Please try again.'
-      redirect_to csv_import_path
+      'Please try again. ' + 
+      'If you continue to have problems please contact me and include your CSV file.'
+      params[:upload].delete(:csv)
+      redirect_to csv_import_path(params[:upload])
     end
-
-    # While under development, just respond by rendering some in-line text.
-    # Send back the request parameters in JSON (JavaScript Object Notation)
-    # format, i.e. something reasonably easy to parse with the human eye.
-    #render :text => params[:upload][:csv].read
   end
 
 
