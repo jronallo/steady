8a11f3bc92aaa1ddaa13af52bfb6eeb52eb08fc7
Jason Ronallo <jronallo@gmail.com>
2011-02-12 09:57:51 -0500

    config changes to mailer to not specify mailer in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,17 @@ Steady::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  # $ export GMAIL_SMTP_USER=username@gmail.com
+  # $ export GMAIL_SMTP_PASSWORD=yourpassword
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'heroku.com',
+    :user_name            => ENV['GMAIL_SMTP_USER'],
+    :password             => ENV['GMAIL_SMTP_PASSWORD'],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
   
 #  config.action_mailer.delivery_method = :sendmail
 #  config.action_mailer.perform_deliveries = true
