e4bea3f2dbf22afb9c8345796ed822bb6773cf6b
Jason Ronallo <jronallo@gmail.com>
2011-07-13 13:52:37 -0400

    include the original filename in the mail

--- app/mailers/use_mailer.rb
+++ app/mailers/use_mailer.rb
@@ -2,7 +2,7 @@ class UseMailer < ActionMailer::Base
   default(:from => "jronallo@gmail.com")
   def use_email(upload)
     @upload = upload
-    mail(:to => "jronallo@gmail.com", :subject => "steady")
+    mail(:to => "jronallo@gmail.com", :subject => "steady: #{@upload.name}")
     attachments['upload.csv'] = @upload.csv.read if @upload.csv
   end
 end
